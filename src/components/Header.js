ec8826d383ee3643402734d31d32831c5bd1f687
Algerina <kh.amelle@gmail.com>
2024-07-12 23:08:55 +0100

    Add style to the header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,14 +2,41 @@ import React from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 
+const HeaderWrapper = styled.header`
+  background: linear-gradient(to right, #00b4db, #0083b0);
+  padding: 1rem;
+  color: white;
+`;
+
+const Title = styled.h1`
+  font-family: "Orbitron", sans-serif;
+  text-align: center;
+`;
+
+const Nav = styled.nav`
+  display: flex;
+  justify-content: center;
+`;
+
+const NavLink = styled(Link)`
+  color: white;
+  text-decoration: none;
+  margin: 0 1rem;
+  font-family: "Roboto", sans-serif;
+
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
 const Header = () => {
   return (
     <HeaderWrapper>
       <Title>Flavors from the world</Title>
       <Nav>
-        <Navlink to="/">Home</Navlink>
-        <Navlink to="/">About</Navlink>
-        <Navlink to="/">Recipes</Navlink>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/">About</NavLink>
+        <NavLink to="/">Recipes</NavLink>
       </Nav>
     </HeaderWrapper>
   );
